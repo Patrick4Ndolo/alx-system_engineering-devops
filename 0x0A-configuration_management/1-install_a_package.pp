6b85897e44e3246c3e697a1c919c1f2d1a99b9cc
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-04-24 13:41:29 +0300

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+#Installs specific version of flask which is 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
+#This script executes a command
 exec { 'killmenow':
-  command => '/usr/bin/pkill killmenow',
-  onlyif  => '/usr/bin/pgrep killmenow',
+  path  => '/usr/bin:/usr/sbin:/bin',
 }
